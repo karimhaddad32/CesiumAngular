384203986f102b0ed6db7ee34eb1d3f8d5cee2b8
Karim Haddad <karim.haddad@hotmail.com>
2020-04-07 15:30:32 -0400

    commit before doing the universal server side app

--- src/app/extent/extent.component.ts
+++ src/app/extent/extent.component.ts
@@ -3,7 +3,7 @@ import { ExtentService } from './../services/extent.service';
 import { Extent } from './../classes/extent';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-extent',
@@ -17,12 +17,17 @@ export class ExtentComponent implements OnInit {
   // ActivatedRoute to get the extension in the URL
   // ExtentService to get the data of the provided extension
   // Router to redirect in case of no response
+  // SharedService is for the page titles
+  // Title is to access the page title.
+  // Meta is to change the meta tags of the website.
+
   constructor(
     private route: ActivatedRoute,
     private extentService: ExtentService,
     private router: Router,
     private titleService: Title,
     private sharedService: SharedService,
+    private meta: Meta
     ) { }
 
   ngOnInit() {
@@ -37,8 +42,37 @@ export class ExtentComponent implements OnInit {
         this.extent = extent;
         this.titleService.setTitle(`${this.extent.name} - ${this.sharedService.cdbTitle}`);
       });
-    }
-    );
+      this.meta.addTags([
+        {
+          name: 'description',
+          content: this.extent.name + ' CDB in Cesium'
+        },
+        {
+          property: 'og:title',
+          content: `${this.extent.name} - ${this.sharedService.cdbTitle}`
+        },
+        {
+          property: 'og:type',
+          content: 'Web-site'
+        },
+        {
+          property: 'og:url',
+          content: this.sharedService.baseUrl + this.extent.name
+        },
+        {
+          property: 'og:image',
+          content: ''
+        },
+        {
+          property: 'og:description',
+          content: this.extent.name + ' CDB in Cesium'
+        },
+        {
+          property: 'og:site_name',
+          content: this.sharedService.cdbTitle
+        }
+      ]);
+    });
   }
 
 }
--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class SharedService {
 
 cdbTitle = 'Presagis - Cesium';
+baseUrl = 'http://localhost:4200/';
 
 constructor() { }
 
