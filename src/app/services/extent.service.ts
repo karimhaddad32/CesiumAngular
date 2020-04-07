1ac23476d15a313f781c8d549ed4df79a8195248
Karim Haddad <karim.haddad@hotmail.com>
2020-04-07 16:37:10 -0400

    REST API calls are prepared

--- src/app/services/extent.service.ts
+++ src/app/services/extent.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { EXTENTS } from './../mock-extents';
 import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -9,20 +10,26 @@ import { Extent } from '../classes/extent';
 export class ExtentService {
 
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
 // Rest API calls will be here
-
+serverUrl = '';
 // Returns only availabe CDBs and their info
   getExtents(): Observable<Extent[]> {
+    // Tempo
     const extents: Extent[] = EXTENTS;
     return of(extents);
+    // Rest API call from the web server.
+    return this.http.get<Extent[]>(this.serverUrl);
   }
 
 // Returns the requested CDB details
   getExtent(name: string): Observable<Extent> {
+    // Tempo
     const extents: Extent[] = EXTENTS.filter(a => a.name === name);
     return of(extents[0]);
+    // Rest API call from the web server.
+    return this.http.get<Extent>(this.serverUrl + '/name');
   }
 
 }
