913d74f706bbaeb7bc0ecf7dc65826ac9e54d83f
Karim Haddad <karim.haddad@hotmail.com>
2020-08-20 15:22:05 -0400

    testing isDevmode

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 
 import { enableProdMode, isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -6,13 +7,14 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 
-if (isDevMode()){
+
+if (window.location.href.includes('localhost')){
   Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
 
 }else{
   Cesium.buildModuleUrl.setBaseUrl('/CesiumAngular/assets/cesium/');
-
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
